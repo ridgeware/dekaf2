a785416de5c8af2700d5b64d2228d6498c3d0908
Joachim Schurig <jschurig@gmx.net>
2018-03-06 18:49:52 +0100

    forgot to commit the new files (khttp_request)

---
+++ khttp_request.cpp
@@ -0,0 +1,97 @@
+/*
+ //-----------------------------------------------------------------------------//
+ //
+ // DEKAF(tm): Lighter, Faster, Smarter (tm)
+ //
+ // Copyright (c) 2018, Ridgeware, Inc.
+ //
+ // +-------------------------------------------------------------------------+
+ // | /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\|
+ // |/+---------------------------------------------------------------------+/|
+ // |/|                                                                     |/|
+ // |\|  ** THIS NOTICE MUST NOT BE REMOVED FROM THE SOURCE CODE MODULE **  |\|
+ // |/|                                                                     |/|
+ // |\|   OPEN SOURCE LICENSE                                               |\|
+ // |/|                                                                     |/|
+ // |\|   Permission is hereby granted, free of charge, to any person       |\|
+ // |/|   obtaining a copy of this software and associated                  |/|
+ // |\|   documentation files (the "Software"), to deal in the              |\|
+ // |/|   Software without restriction, including without limitation        |/|
+ // |\|   the rights to use, copy, modify, merge, publish,                  |\|
+ // |/|   distribute, sublicense, and/or sell copies of the Software,       |/|
+ // |\|   and to permit persons to whom the Software is furnished to        |\|
+ // |/|   do so, subject to the following conditions:                       |/|
+ // |\|                                                                     |\|
+ // |/|   The above copyright notice and this permission notice shall       |/|
+ // |\|   be included in all copies or substantial portions of the          |\|
+ // |/|   Software.                                                         |/|
+ // |\|                                                                     |\|
+ // |/|   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY         |/|
+ // |\|   KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE        |\|
+ // |/|   WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR           |/|
+ // |\|   PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS        |\|
+ // |/|   OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR          |/|
+ // |\|   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR        |\|
+ // |/|   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE         |/|
+ // |\|   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.            |\|
+ // |/|                                                                     |/|
+ // |/+---------------------------------------------------------------------+/|
+ // |\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ |
+ // +-------------------------------------------------------------------------+
+ */
+
+#include "khttp_request.h"
+
+namespace dekaf2 {
+
+
+//-----------------------------------------------------------------------------
+bool KHTTPRequest::Parse(KInStream& Stream)
+//-----------------------------------------------------------------------------
+{
+	KString sLine;
+
+	if (!Stream.ReadLine(sLine) || sLine.empty())
+	{
+		return false;
+	}
+
+	// analyze method and resource
+	// GET /some/page HTTP/1.1
+
+	std::vector<KStringView> Words;
+	Words.reserve(3);
+	kSplit(Words, sLine, " ");
+
+	if (Words.size() != 3)
+	{
+		// garbage, bail out
+		return false;
+	}
+
+	m_Method = Words[0];
+	m_Resource = Words[1];
+	m_HTTPVersion = Words[2];
+
+	return KHTTPHeader::Parse(Stream);
+
+} // Parse
+
+//-----------------------------------------------------------------------------
+bool KHTTPRequest::Serialize(KOutStream& Stream)
+//-----------------------------------------------------------------------------
+{
+
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+void KHTTPRequest::clear()
+//-----------------------------------------------------------------------------
+{
+	KHTTPHeader::clear();
+	m_Resource.clear();
+	m_HTTPVersion.clear();
+}
+
+} // end of namespace dekaf2
---
+++ khttp_request.h
@@ -0,0 +1,91 @@
+/*
+ //-----------------------------------------------------------------------------//
+ //
+ // DEKAF(tm): Lighter, Faster, Smarter (tm)
+ //
+ // Copyright (c) 2018, Ridgeware, Inc.
+ //
+ // +-------------------------------------------------------------------------+
+ // | /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\|
+ // |/+---------------------------------------------------------------------+/|
+ // |/|                                                                     |/|
+ // |\|  ** THIS NOTICE MUST NOT BE REMOVED FROM THE SOURCE CODE MODULE **  |\|
+ // |/|                                                                     |/|
+ // |\|   OPEN SOURCE LICENSE                                               |\|
+ // |/|                                                                     |/|
+ // |\|   Permission is hereby granted, free of charge, to any person       |\|
+ // |/|   obtaining a copy of this software and associated                  |/|
+ // |\|   documentation files (the "Software"), to deal in the              |\|
+ // |/|   Software without restriction, including without limitation        |/|
+ // |\|   the rights to use, copy, modify, merge, publish,                  |\|
+ // |/|   distribute, sublicense, and/or sell copies of the Software,       |/|
+ // |\|   and to permit persons to whom the Software is furnished to        |\|
+ // |/|   do so, subject to the following conditions:                       |/|
+ // |\|                                                                     |\|
+ // |/|   The above copyright notice and this permission notice shall       |/|
+ // |\|   be included in all copies or substantial portions of the          |\|
+ // |/|   Software.                                                         |/|
+ // |\|                                                                     |\|
+ // |/|   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY         |/|
+ // |\|   KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE        |\|
+ // |/|   WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR           |/|
+ // |\|   PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS        |\|
+ // |/|   OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR          |/|
+ // |\|   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR        |\|
+ // |/|   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE         |/|
+ // |\|   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.            |\|
+ // |/|                                                                     |/|
+ // |/+---------------------------------------------------------------------+/|
+ // |\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ |
+ // +-------------------------------------------------------------------------+
+ */
+
+#pragma once
+
+#include "kstringview.h"
+#include "kcasestring.h"
+#include "kprops.h"
+#include "khttp_method.h"
+#include "khttp_header.h"
+#include "kurl.h"
+
+namespace dekaf2 {
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+	class KHTTPRequest : public KHTTPHeader
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+{
+
+//------
+public:
+//------
+
+	using KHeaderMap = KProps<KCaseTrimString, KString>; // case insensitive map for header info
+
+	//-----------------------------------------------------------------------------
+	bool Parse(KInStream& Stream);
+	//-----------------------------------------------------------------------------
+
+	//-----------------------------------------------------------------------------
+	bool Serialize(KOutStream& Stream);
+	//-----------------------------------------------------------------------------
+
+	//-----------------------------------------------------------------------------
+	void clear();
+	//-----------------------------------------------------------------------------
+
+//------
+protected:
+//------
+
+//------
+private:
+//------
+
+	detail::http::KMethod m_Method;
+	KURI m_Resource;
+	KString m_HTTPVersion;
+
+}; // KHTTPRequest
+
+} // end of namespace dekaf2

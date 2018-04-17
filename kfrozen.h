3b239dfbc542d940e4e33f5edd80ac365b32ba26
Joachim Schurig <jschurig@gmx.net>
2018-04-17 16:04:48 +0200

    removed frozen::hash(KString), as a frozen container can never be constructed from a non-constexpr KString

--- kfrozen.h
+++ kfrozen.h
@@ -55,7 +55,7 @@
 
 namespace frozen {
 
-// add hash specializations for KStringView and KString to frozen:: - the easier
+// add hash specialization for KStringView to frozen:: - the easier
 // the better, the compiler will find the seed needed for a perfect hash anyway
 
 template <>
@@ -72,31 +72,6 @@ struct elsa<dekaf2::KStringView>
 	}
 
 	constexpr std::size_t operator()(dekaf2::KStringView value, std::size_t seed) const
-	{
-		std::size_t d = seed;
-		for (std::size_t i = 0; i < value.size(); ++i)
-		{
-			d = (d * 0x01000193) ^ value[i];
-		}
-		return d;
-	}
-
-};
-
-template <>
-struct elsa<dekaf2::KString>
-{
-	constexpr std::size_t operator()(const dekaf2::KString& value) const
-	{
-		std::size_t d = 5381;
-		for (std::size_t i = 0; i < value.size(); ++i)
-		{
-			d = d * 33 + value[i];
-		}
-		return d;
-	}
-
-	constexpr std::size_t operator()(const dekaf2::KString& value, std::size_t seed) const
 	{
 		std::size_t d = seed;
 		for (std::size_t i = 0; i < value.size(); ++i)

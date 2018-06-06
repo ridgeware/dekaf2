eba6d71b2a28a2cd3da86bd80dafd5bbbde0a08c
Joachim Schurig <jschurig@gmx.net>
2018-06-06 13:43:36 +0200

    adding unit tests for BLAKE2

--- utests/kmessagedigest_tests.cpp
+++ utests/kmessagedigest_tests.cpp
@@ -18,6 +18,10 @@ TEST_CASE("KMessageDigest") {
 		CHECK ( sha384 == "38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b" );
 		KSHA512 sha512;
 		CHECK ( sha512 == "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e" );
+#ifdef DEKAF2_HAS_BLAKE2
+		KBLAKE2 blake2;
+		CHECK ( blake2 == "786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce");
+#endif
 	}
 
 	SECTION("known digests for nonempty strings")
@@ -28,6 +32,11 @@ TEST_CASE("KMessageDigest") {
 		sha224.clear();
 		sha224.Update("The quick brown fox jumps over the lazy dog.");
 		CHECK ( sha224 == "619cba8e8e05826e9b8c519c0a5c68f4fb653e8a3d8aa04bb2c8cd4c" );
+#ifdef DEKAF2_HAS_BLAKE2
+		KBLAKE2 blake2;
+		blake2.Update("The quick brown fox jumps over the lazy dog");
+		CHECK ( blake2 == "a8add4bdddfd93e4877d2746e62817b116364a1fa7bc148d95090bc7333b3673f82401cf7aa2e4cb1ecd90296e3f14cb5413f8ed77be73045b13914cdcd6a918");
+#endif
 	}
 
 	SECTION("known digests for nonempty strings, multiple updates")

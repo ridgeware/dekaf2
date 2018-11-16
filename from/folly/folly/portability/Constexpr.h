22efb6c14502f47eb6453e107519b6c3e3d801e0
Joachim Schurig <jschurig@gmx.net>
2018-11-16 12:32:33 +0100

    less stack consuming constexpr strlen

--- from/folly/folly/portability/Constexpr.h
+++ from/folly/folly/portability/Constexpr.h
@@ -24,18 +24,40 @@ namespace folly {
 
 namespace detail {
 
+// A constexpr std::char_traits::length replacement for pre-C++17.
+#if (__cplusplus >= 201402L)
+// >= C++14
 template <typename Char>
-constexpr size_t constexpr_strlen_internal(const Char* s, size_t len) {
-  return *s == Char(0) ? len : constexpr_strlen_internal(s + 1, len + 1);
+constexpr size_t constexpr_strlen_internal(const Char *s)
+{
+  const Char *start = s;
+  while (*s) ++s;
+  return s - start;
 }
+#else
+// C++11
+template <typename Char>
+constexpr size_t constexpr_strlen_internal_1(const Char* s, size_t len)
+{
+  return *s == Char(0) ? len : constexpr_strlen_internal_1(s + 1, len + 1);
+}
+
+template <typename Char>
+constexpr size_t constexpr_strlen_internal(const Char* s)
+{
+  return constexpr_strlen_internal_1(s, 0);
+}
+#endif
+
 static_assert(
-    constexpr_strlen_internal("123456789", 0) == 9,
+    constexpr_strlen_internal("123456789") == 9,
     "Someone appears to have broken constexpr_strlen...");
+
 } // namespace detail
 
 template <typename Char>
 constexpr size_t constexpr_strlen(const Char* s) {
-  return detail::constexpr_strlen_internal(s, 0);
+	return detail::constexpr_strlen_internal(s);
 }
 
 template <>
@@ -43,15 +65,16 @@ constexpr size_t constexpr_strlen(const char* s) {
 #if defined(__clang__)
   return s ? __builtin_strlen(s) : 0;
 #elif defined(_MSC_VER) || defined(__CUDACC__)
-  return detail::constexpr_strlen_internal(s, 0);
+  return detail::constexpr_strlen_internal(s);
 #elif defined(__GNUC__)
   #if (__GNUC__ > 7)
 	return s ? std::char_traits<char>::length(s) : 0;
   #else
-	return s ? detail::constexpr_strlen_internal(s, 0) : 0;
+    return s ? detail::constexpr_strlen_internal(s) : 0;
   #endif
 #else
   return std::strlen(s);
 #endif
 }
-}
+
+} // namespace folly

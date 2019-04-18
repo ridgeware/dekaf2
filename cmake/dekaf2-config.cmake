6a68f54196f932b091e6c792f6c27f3803246f40
Joachim Schurig <jschurig@gmx.net>
2019-04-18 11:26:17 +0200

    make Release the default lib version

--- cmake/dekaf2-config.cmake
+++ cmake/dekaf2-config.cmake
@@ -1,5 +1,10 @@
 # dekaf2-config.cmake - package configuration file
 
+# if no build type is used, pick the release version of the library
+if (NOT CMAKE_BUILD_TYPE)
+	set(CMAKE_BUILD_TYPE "Release")
+endif()
+
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/dekaf2.cmake)
 

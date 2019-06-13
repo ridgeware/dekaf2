ee281ce99dfbf6db1b4eceea3a29de8724e8fb3e
Joachim Schurig <jschurig@translations.com>
2019-06-13 14:13:03 +0200

    marking Property methods as constexpr, adding unit tests

--- utests/kctype_tests.cpp
+++ utests/kctype_tests.cpp
@@ -6,15 +6,15 @@
 
 #ifndef DEKAF2_IS_WINDOWS
 
-// These tests cannot work on Windows, which is basically the reason
-// why kctype.h was written.. And actually they do not work on any
-// system without failures, as every lib implements a different subset
-// of character properties..
-
 using namespace dekaf2;
 
 TEST_CASE("KCType")
 {
+	// These tests cannot work on Windows, which is basically the reason
+	// why kctype.h was written.. And actually they do not work on any
+	// system without failures, as every lib implements a different subset
+	// of character properties..
+
 	/*
 	SECTION("IsSpace")
 	{
@@ -61,6 +61,86 @@ TEST_CASE("KCType")
 		}
 	}
 */
+
+	// tests that have to work on all systems
+
+	SECTION("signed chars")
+	{
+		for (char ch = -0x7f; ch < 0x7f; ++ch)
+		{
+			bool bIsUpper = ch >= 'A' && ch <= 'Z';
+			CHECK ( KASCII::kIsUpper(ch) == bIsUpper );
+		}
+		for (char ch = -0x7f; ch < 0x7f; ++ch)
+		{
+			bool bIsLower = ch >= 'a' && ch <= 'z';
+			CHECK ( KASCII::kIsLower(ch) == bIsLower );
+		}
+		for (char ch = -0x7f; ch < 0x7f; ++ch)
+		{
+			bool bIsAlpha = (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+			CHECK ( KASCII::kIsAlpha(ch) == bIsAlpha );
+		}
+		for (char ch = -0x7f; ch < 0x7f; ++ch)
+		{
+			bool bIsDigit = ch >= '0' && ch <= '9';
+			CHECK ( KASCII::kIsDigit(ch) == bIsDigit );
+		}
+		for (char ch = -0x7f; ch < 0x7f; ++ch)
+		{
+			bool bIsXDigit = (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F');
+			CHECK ( KASCII::kIsXDigit(ch) == bIsXDigit );
+		}
+		for (char ch = -0x7f; ch < 0x7f; ++ch)
+		{
+			bool bIsBlank = ch == ' ' || ch == '\t';
+			CHECK ( KASCII::kIsBlank(ch) == bIsBlank );
+		}
+		for (char ch = -0x7f; ch < 0x7f; ++ch)
+		{
+			bool bIsSpace = (ch >= 0x09 && ch <= 0x0e) || ch == 0x20;
+			CHECK ( KASCII::kIsSpace(ch) == bIsSpace );
+		}
+	}
+
+	SECTION("unsigned chars")
+	{
+		for (unsigned char ch = 0; ch < 0xff; ++ch)
+		{
+			bool bIsUpper = ch >= 'A' && ch <= 'Z';
+			CHECK ( KASCII::kIsUpper(ch) == bIsUpper );
+		}
+		for (unsigned char ch = 0; ch < 0xff; ++ch)
+		{
+			bool bIsLower = ch >= 'a' && ch <= 'z';
+			CHECK ( KASCII::kIsLower(ch) == bIsLower );
+		}
+		for (unsigned char ch = 0; ch < 0xff; ++ch)
+		{
+			bool bIsAlpha = (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+			CHECK ( KASCII::kIsAlpha(ch) == bIsAlpha );
+		}
+		for (unsigned char ch = 0; ch < 0xff; ++ch)
+		{
+			bool bIsDigit = ch >= '0' && ch <= '9';
+			CHECK ( KASCII::kIsDigit(ch) == bIsDigit );
+		}
+		for (unsigned char ch = 0; ch < 0xff; ++ch)
+		{
+			bool bIsXDigit = (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F');
+			CHECK ( KASCII::kIsXDigit(ch) == bIsXDigit );
+		}
+		for (unsigned char ch = 0; ch < 0xff; ++ch)
+		{
+			bool bIsBlank = ch == ' ' || ch == '\t';
+			CHECK ( KASCII::kIsBlank(ch) == bIsBlank );
+		}
+		for (unsigned char ch = 0; ch < 0xff; ++ch)
+		{
+			bool bIsSpace = (ch >= 0x09 && ch <= 0x0e) || ch == 0x20;
+			CHECK ( KASCII::kIsSpace(ch) == bIsSpace );
+		}
+	}
 }
 
 #endif // Windows

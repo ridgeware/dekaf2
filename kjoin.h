6328d69c46616b61f67fdb8d2ed646311303d418
Joachim Schurig <jschurig@translations.com>
2019-06-19 16:51:15 +0200

    adding kJoin templates for sequential and associative containers

---
+++ kjoin.h
@@ -0,0 +1,124 @@
+/*
+//
+// DEKAF(tm): Lighter, Faster, Smarter(tm)
+//
+// Copyright (c) 2019, Ridgeware, Inc.
+//
+// +-------------------------------------------------------------------------+
+// | /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\|
+// |/+---------------------------------------------------------------------+/|
+// |/|                                                                     |/|
+// |\|  ** THIS NOTICE MUST NOT BE REMOVED FROM THE SOURCE CODE MODULE **  |\|
+// |/|                                                                     |/|
+// |\|   OPEN SOURCE LICENSE                                               |\|
+// |/|                                                                     |/|
+// |\|   Permission is hereby granted, free of charge, to any person       |\|
+// |/|   obtaining a copy of this software and associated                  |/|
+// |\|   documentation files (the "Software"), to deal in the              |\|
+// |/|   Software without restriction, including without limitation        |/|
+// |\|   the rights to use, copy, modify, merge, publish,                  |\|
+// |/|   distribute, sublicense, and/or sell copies of the Software,       |/|
+// |\|   and to permit persons to whom the Software is furnished to        |\|
+// |/|   do so, subject to the following conditions:                       |/|
+// |\|                                                                     |\|
+// |/|   The above copyright notice and this permission notice shall       |/|
+// |\|   be included in all copies or substantial portions of the          |\|
+// |/|   Software.                                                         |/|
+// |\|                                                                     |\|
+// |/|   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY         |/|
+// |\|   KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE        |\|
+// |/|   WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR           |/|
+// |\|   PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS        |\|
+// |/|   OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR          |/|
+// |\|   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR        |\|
+// |/|   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE         |/|
+// |\|   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.            |\|
+// |/|                                                                     |/|
+// |/+---------------------------------------------------------------------+/|
+// |\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ |
+// +-------------------------------------------------------------------------+
+//
+*/
+
+#pragma once
+
+#include "kstringview.h"
+#include <type_traits>
+
+/// @file kjoin.h
+/// Highly configurable join template.
+
+namespace dekaf2
+{
+
+namespace detail {
+
+	// primary template handles types that have no key_type member:
+template< class, class = std::void_t<> >
+struct has_key_type : std::false_type { };
+
+// specialization recognizes types that do have a key_type member:
+template< class T >
+struct has_key_type<T, std::void_t<typename T::key_type>> : std::true_type { };
+
+}
+
+//-----------------------------------------------------------------------------
+/// join for sequential containers
+template<typename Container, typename Result,
+	typename std::enable_if_t<detail::has_key_type<Container>::value == false, int> = 0 >
+void kJoin (Result& sBuffer,
+			const Container& ctContainer,
+			KStringView svDelim = ",",
+			// we add the svPairDelim here to give the same interface as for
+			// the associative containers - the value is not used
+			KStringView svPairDelim = "="
+)
+//-----------------------------------------------------------------------------
+{
+	auto it = ctContainer.begin();
+	auto ie = ctContainer.end();
+
+	for (;;)
+	{
+		sBuffer += *it;
+
+		if (++it == ie)
+		{
+			break;
+		}
+
+		sBuffer += svDelim;
+	}
+}
+
+//-----------------------------------------------------------------------------
+/// join for associative containers
+template<typename Container, typename Result,
+	typename std::enable_if_t<detail::has_key_type<Container>::value == true, int> = 0 >
+void kJoin (Result& sBuffer,
+			const Container& ctContainer,
+			KStringView svDelim = ",",
+			KStringView svPairDelim = "="
+)
+//-----------------------------------------------------------------------------
+{
+	auto it = ctContainer.begin();
+	auto ie = ctContainer.end();
+
+	for (;;)
+	{
+		sBuffer += it->first;
+		sBuffer += svPairDelim;
+		sBuffer += it->second;
+
+		if (++it == ie)
+		{
+			break;
+		}
+
+		sBuffer += svDelim;
+	}
+}
+
+} // namespace dekaf2
---
+++ utests/kjoin_tests.cpp
@@ -0,0 +1,75 @@
+#include "catch.hpp"
+#include <dekaf2/kjoin.h>
+#include <dekaf2/kprops.h>
+#include <dekaf2/kstring.h>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <list>
+
+
+using namespace dekaf2;
+
+TEST_CASE("kJoin")
+{
+	SECTION("vector")
+	{
+		std::vector<KStringView> vec {
+			"one",
+			"two",
+			"three",
+			"four"
+		};
+
+		KString sResult;
+		kJoin(sResult, vec);
+
+		CHECK (sResult == "one,two,three,four" );
+	}
+
+	SECTION("list")
+	{
+		std::list<KStringView> list {
+			"one",
+			"two",
+			"three",
+			"four"
+		};
+
+		KString sResult;
+		kJoin(sResult, list);
+
+		CHECK (sResult == "one,two,three,four" );
+	}
+
+	SECTION("map")
+	{
+		std::map<KStringView, KStringView> map {
+			{ "1" , "one"   },
+			{ "2" , "two"   },
+			{ "3" , "three" },
+			{ "4" , "four"  }
+		};
+
+		KString sResult;
+		kJoin(sResult, map);
+
+		CHECK (sResult == "1=one,2=two,3=three,4=four" );
+	}
+
+	SECTION("kprops")
+	{
+		KProps<KStringView, KStringView, true, true> map {
+			{ "1" , "one"   },
+			{ "2" , "two"   },
+			{ "3" , "three" },
+			{ "4" , "four"  }
+		};
+
+		KString sResult;
+		kJoin(sResult, map);
+
+		CHECK (sResult == "1=one,2=two,3=three,4=four" );
+	}
+
+}

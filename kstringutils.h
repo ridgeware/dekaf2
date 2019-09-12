2dd843a31b1d2cc61d05e722952a28e284fb2463
Joachim Schurig <jschurig@translations.com>
2019-09-12 19:59:52 +0200

    one more fix for old gccs that are not fully C++17 compatible

--- kstringutils.h
+++ kstringutils.h
@@ -428,7 +428,7 @@ String kFormNumber(Arithmetic i, typename String::value_type separator = ',', ty
 
 	DEKAF2_TRY
 	{
-#ifdef DEKAF2_HAS_CPP_17
+#if defined(DEKAF2_HAS_CPP_17) && (DEKAF2_NO_GCC || DEKAF2_GCC_VERSION >= 80000)
 		if constexpr (std::is_same<String, KString>::value)
 		{
 			result = KString::to_string(i);

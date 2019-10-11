9c36e5dbdfe9b6804b36c19ea1b974dfdaed4f4a
Joachim Schurig <jschurig@translations.com>
2019-10-11 11:47:21 +0200

    more rest client unit tests

--- utests/krestclient_tests.cpp
+++ utests/krestclient_tests.cpp
@@ -2,13 +2,14 @@
 
 #include <dekaf2/krestclient.h>
 #include <dekaf2/krest.h>
+#include <dekaf2/kmessagedigest.h>
 
 using namespace dekaf2;
 
 void rest_login(KRESTServer& REST)
 {
 	if (REST.GetQueryParm("user") == "Jason"
-		&& REST.GetQueryParm("pass") == "secret")
+		&& REST.GetQueryParm("pass") == KSHA256("secret").Digest())
 	{
 		REST.json.tx =
 		{
@@ -22,12 +23,40 @@ void rest_login(KRESTServer& REST)
 	}
 }
 
-void rest_help(KRESTServer& REST)
+void rest_settings(KRESTServer& REST)
 {
+	if (REST.Request.Headers.Get(KHTTPHeaders::AUTHORIZATION) != "1234567890")
+	{
+		throw KHTTPError { KHTTPError::H4xx_NOTAUTH, "not authenticated" };
+	}
+
+	if (REST.GetQueryParm(":NAME") == "Jason")
+	{
+		REST.json.tx = REST.json.rx;
+		REST.SetStatus(KHTTPError::H2xx_CREATED);
+	}
+	else
+	{
+		throw KHTTPError { KHTTPError::H4xx_NOTFOUND, "unknown user" };
+	}
 }
 
 void rest_patch(KRESTServer& REST)
 {
+	if (REST.Request.Headers.Get(KHTTPHeaders::AUTHORIZATION) != "1234567890")
+	{
+		throw KHTTPError { KHTTPError::H4xx_NOTAUTH, "not authenticated" };
+	}
+
+	if (REST.GetQueryParm(":NAME") == "Jason")
+	{
+		REST.json.tx = REST.json.rx;
+		REST.SetStatus(KHTTPError::H2xx_UPDATED);
+	}
+	else
+	{
+		throw KHTTPError { KHTTPError::H4xx_NOTFOUND, "unknown user" };
+	}
 }
 
 TEST_CASE("KRESTCLIENT")
@@ -41,9 +70,9 @@ TEST_CASE("KRESTCLIENT")
 
 		KRESTRoutes::FunctionTable RTable[]
 		{
-			{ "GET",   false, "/login",              rest_login },
-			{ "POST",  false, "/help",               rest_help  },
-			{ "PATCH", false, "/user/:NAME/address", rest_patch },
+			{ "GET",   false, "/login",              rest_login    },
+			{ "POST",  false, "/user/:NAME",         rest_settings },
+			{ "PATCH", false, "/user/:NAME/address", rest_patch    },
 		};
 
 		KRESTRoutes Routes;
@@ -64,7 +93,7 @@ TEST_CASE("KRESTCLIENT")
 		auto oResponse = Host.Get("login")
 							 .SetError(ec)
 		                     .AddQuery("user", "Tom")
-		                     .AddQuery("pass", "Jerry")
+		                     .AddQuery("pass", KSHA256("Jerry").Digest())
 		                     .Request();
 
 		CHECK ( ec.value() == 401 );
@@ -79,7 +108,7 @@ TEST_CASE("KRESTCLIENT")
 		oResponse = Host.Post("login")
 						 .SetError(ec)
 						 .AddQuery("user", "Tom")
-						 .AddQuery("pass", "Jerry")
+						 .AddQuery("pass", KSHA256("Jerry").Digest())
 						 .Request();
 
 		CHECK ( ec.value() == 405 );
@@ -94,16 +123,97 @@ TEST_CASE("KRESTCLIENT")
 		oResponse = Host.Get("login")
 						 .SetError(ec)
 						 .AddQuery("user", "Jason")
-						 .AddQuery("pass", "secret")
+						 .AddQuery("pass", KSHA256("secret").Digest())
 						 .Request();
 
 		CHECK ( ec.value() == 0 );
 		CHECK ( ec == false );
-		CHECK ( Host.HttpSuccess() == true  );
-		CHECK ( Host.HttpFailure() == false );
-		CHECK ( oResponse.empty()  == false );
+		CHECK ( Host.GetStatusCode() == 200  );
+		CHECK ( Host.HttpSuccess()  == true  );
+		CHECK ( Host.HttpFailure()  == false );
+		CHECK ( oResponse.empty()   == false );
 		CHECK ( kjson::GetStringRef(oResponse, "token")   == "1234567890" );
 		CHECK ( kjson::GetStringRef(oResponse, "expires") == "then"       );
+
+		KString sToken = kjson::GetStringRef(oResponse, "token");
+
+		oResponse = Host.Post("user/Tom")
+		                .AddHeader(KHTTPHeaders::AUTHORIZATION, "12345")
+		                .SetError(ec)
+		                .Request(
+		{
+			{ "city"     , "Berlin"     },
+			{ "street"   , "Tauentzien" },
+			{ "number"   , "73a"        },
+			{ "zip"      , "10241"      }
+		});
+
+		CHECK ( ec.value() == 401 );
+		CHECK ( ec == true );
+		CHECK ( ec.message() == "POST user/Tom: HTTP-401 NOT AUTHORIZED - not authenticated" );
+		CHECK ( Host.HttpSuccess() == false );
+		CHECK ( Host.HttpFailure() == true  );
+		CHECK ( oResponse.empty()  == false );
+		CHECK ( kjson::GetStringRef(oResponse, "message") == "not authenticated" );
+
+		oResponse = Host.Post("user/Tom")
+		                .AddHeader(KHTTPHeaders::AUTHORIZATION, "1234567890")
+		                .SetError(ec)
+		                .Request(
+		{
+			{ "city"     , "Berlin"     },
+			{ "street"   , "Tauentzien" },
+			{ "number"   , "73a"        },
+			{ "zip"      , "10241"      }
+		});
+
+		CHECK ( ec.value() == 404 );
+		CHECK ( ec == true );
+		CHECK ( ec.message() == "POST user/Tom: HTTP-404 NOT FOUND - unknown user" );
+		CHECK ( Host.HttpSuccess() == false );
+		CHECK ( Host.HttpFailure() == true  );
+		CHECK ( oResponse.empty()  == false );
+		CHECK ( kjson::GetStringRef(oResponse, "message") == "unknown user" );
+
+		oResponse = Host.Post("user/Jason")
+		                .AddHeader(KHTTPHeaders::AUTHORIZATION, sToken)
+		                .SetError(ec)
+		                .Request(
+		{
+			{ "city"     , "Berlin"     },
+			{ "street"   , "Tauentzien" },
+			{ "number"   , "73a"        },
+			{ "zip"      , "10241"      }
+		});
+
+		CHECK ( ec.value() == 0 );
+		CHECK ( ec == false );
+		CHECK ( Host.GetStatusCode() == 201  );
+		CHECK ( Host.HttpSuccess()  == true  );
+		CHECK ( Host.HttpFailure()  == false );
+		CHECK ( oResponse.empty()   == false );
+		CHECK ( kjson::GetStringRef(oResponse, "city") == "Berlin" );
+		CHECK ( kjson::GetStringRef(oResponse, "zip" ) == "10241"  );
+
+		oResponse = Host.Patch("user/Jason/address")
+		                .AddHeader(KHTTPHeaders::AUTHORIZATION, sToken)
+		                .SetError(ec)
+		                .Request(
+		{
+			{ "city"     , "Berlin"         },
+			{ "street"   , "Bergmannstraße" },
+			{ "number"   , "67"             },
+			{ "zip"      , "10641"          }
+		});
+
+		CHECK ( ec.value() == 0 );
+		CHECK ( ec == false );
+		CHECK ( Host.GetStatusCode() == 201  );
+		CHECK ( Host.HttpSuccess()  == true  );
+		CHECK ( Host.HttpFailure()  == false );
+		CHECK ( oResponse.empty()   == false );
+		CHECK ( kjson::GetStringRef(oResponse, "city") == "Berlin" );
+		CHECK ( kjson::GetStringRef(oResponse, "zip" ) == "10641"  );
 
 	}
 }

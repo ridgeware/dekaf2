4a89eea602e8b52d0c18aa5b5f16b02ce4e4469f
Joachim Schurig <jschurig@translations.com>
2019-11-04 14:20:35 +0100

    initial test for kgetruntimestack.cpp

---
+++ utests/kgetruntimestack_tests.cpp
@@ -0,0 +1,20 @@
+#include "catch.hpp"
+#include <dekaf2/kgetruntimestack.h>
+#include <dekaf2/kstring.h>
+
+#ifndef DEKAF2_IS_WINDOWS
+
+using namespace dekaf2;
+
+TEST_CASE("kGetRuntimeStack")
+{
+	SECTION("backtrace")
+	{
+		KString sTrace = kGetRuntimeStack();
+		KString sExpected;
+		CHECK ( sTrace.Split("\n", "").size() > 6 );
+	}
+
+}
+
+#endif

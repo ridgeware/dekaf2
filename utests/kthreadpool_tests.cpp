b7c4a3f852a61901aec88339b57ec99ea3328326
Joachim Schurig <jschurig@gmx.net>
2021-03-01 13:33:36 +0100

    final cleanup for windows packaged task issues

--- utests/kthreadpool_tests.cpp
+++ utests/kthreadpool_tests.cpp
@@ -27,6 +27,26 @@ TEST_CASE("KThreadPool")
 		i1 = 1;
 	});
 
+	MyClass C(10);
+
+	auto future3 = Queue.push(&MyClass::Inc, &C, 3);
+	auto future4 = Queue.push(&MyClass::Dec, &C);
+
+	auto w1 = future1.wait_for(std::chrono::seconds(2));
+	auto w3 = future3.wait_for(std::chrono::seconds(2));
+	auto w4 = future4.wait_for(std::chrono::seconds(2));
+
+	CHECK ( w1 == std::future_status::ready   );
+	CHECK ( w3 == std::future_status::ready   );
+	CHECK ( w4 == std::future_status::ready   );
+
+	CHECK ( i1 == 1 );
+	CHECK ( future1.valid() );
+
+#ifndef DEKAF2_IS_WINDOWS
+	// VS 2017 has issues compiling this - it is clearly
+	// not an implementation problem of dekaf2, therefore
+	// we simply drop the test for windows..
 	auto future2 = Queue.push([&i2]() -> KString
 	{
 		std::this_thread::sleep_for(std::chrono::milliseconds(200));
@@ -34,38 +54,16 @@ TEST_CASE("KThreadPool")
 		return "done";
 	});
 
-	MyClass C(10);
-
-	auto future3 = Queue.push(&MyClass::Inc, &C, 3);
-	auto future4 = Queue.push(&MyClass::Dec, &C);
-
-	auto w1 = future1.wait_for(std::chrono::seconds(2));
 	auto w2 = future2.wait_for(std::chrono::seconds(2));
-	auto w3 = future3.wait_for(std::chrono::seconds(2));
-	auto w4 = future4.wait_for(std::chrono::seconds(2));
-
-#ifndef DEKAF2_IS_WINDOWS
-	// VS 2017 has issues compiling this - it is clearly
-	// not an implementation problem of dekaf2, therefore
-	// we simply drop the test for windows..
 	auto future5 = Queue.push(&MyClass::Add, &C, 5);
 	auto w5 = future5.wait_for(std::chrono::seconds(2));
-#endif
 
-	CHECK ( w1 == std::future_status::ready   );
 	CHECK ( w2 == std::future_status::ready   );
-	CHECK ( w3 == std::future_status::ready   );
-	CHECK ( w4 == std::future_status::ready   );
-#ifndef DEKAF2_IS_WINDOWS
 	CHECK ( w5 == std::future_status::ready   );
-#endif
-	
-	CHECK ( i1 == 1 );
 	CHECK ( i2 == 2 );
-	CHECK ( future1.valid() );
 	CHECK ( future2.valid() );
+	CHECK ( future5.valid() );
 	CHECK ( future2.get() == "done" );
-#ifndef DEKAF2_IS_WINDOWS
 	CHECK ( future5.get() == 17 );
 #endif
 

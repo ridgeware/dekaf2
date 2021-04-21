9d4cf795d3b3512e9a6d33898dd772be39e99f0b
Joachim Schurig <jschurig@gmx.net>
2021-04-21 23:55:27 +0200

    KTCPServer: fixing issue when key and cert are not merged into same file

--- ktcpserver.cpp
+++ ktcpserver.cpp
@@ -518,7 +518,7 @@ bool KTCPServer::LoadSSLCertificates(KStringViewZ sCert, KStringViewZ sKey, KStr
 	m_sPassword = sPassword;
 	m_sCert.clear();
 	m_sKey.clear();
-	return kReadAll(sCert, m_sCert) && (m_sKey.empty() || kReadAll(sKey, m_sKey));
+	return kReadAll(sCert, m_sCert) && (sKey.empty() || kReadAll(sKey, m_sKey));
 
 } // SetSSLCertificateFiles
 

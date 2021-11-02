3c30f2e718ce6c2dd2ff3baa3341dfe1cc3f823b
Joachim Schurig <jschurig@gmx.net>
2021-11-02 14:43:42 +0100

    adapting KSQL smoke tests to changed diff code

--- smoketests/ksql_smoketests.cpp
+++ smoketests/ksql_smoketests.cpp
@@ -1330,7 +1330,9 @@ TEST_CASE("KSQL")
 		auto iChanges = db.DiffSchemas(jSchema1, jSchema3, jDiff, sDiff);
 		CHECK ( iChanges == 2 );
 		CHECK ( sDiff == R"(TESTSCHEMA22_KSQL <-- only in right schema
+
 TESTSCHEMA2_KSQL <-- only in right schema
+
 )" );
 //		KOut.WriteLine(sDiff);
 
@@ -1353,10 +1355,11 @@ TESTSCHEMA2_KSQL <-- only in right schema
 
 		iChanges = db.DiffSchemas(jSchema1, jSchema2, jDiff, sDiff);
 		CHECK ( iChanges == 2 );
-		CHECK ( sDiff == R"(TESTSCHEMA1_KSQL < left schema > right schema
+		CHECK ( sDiff == R"(TESTSCHEMA1_KSQL left schema vs. right schema
 < astring char(100) COLLATE utf8mb4_unicode_ci NOT NULL
 > astring char(10) COLLATE utf8mb4_unicode_ci NOT NULL
 > newstring char(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+
 )" );
 //		KOut.WriteLine(sDiff);
 

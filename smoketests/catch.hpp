9847746dd362901686ac06c3ea09c93376b4a447
Joachim Schurig <jschurig@gmx.net>
2022-05-05 19:10:29 +0200

    fixing catch for Apple ARM

--- smoketests/catch.hpp
+++ smoketests/catch.hpp
@@ -2100,8 +2100,10 @@ namespace Catch{
         #define CATCH_TRAP() \
                 __asm__("li r0, 20\nsc\nnop\nli r0, 37\nli r4, 2\nsc\nnop\n" \
                 : : : "memory","r0","r3","r4" )
-    #else
-        #define CATCH_TRAP() __asm__("int $3\n" : : )
+    #elif defined(__i386__) || defined(__x86_64__)
+        #define CATCH_TRAP() __asm__("int $3\n" : : ) /* NOLINT */
+    #elif defined(__aarch64__)
+        #define CATCH_TRAP()  __asm__(".inst 0xd4200000")
     #endif
 
 #elif defined(CATCH_PLATFORM_LINUX)

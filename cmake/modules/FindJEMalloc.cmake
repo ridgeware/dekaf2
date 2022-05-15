ebaab7fdf36acd31036e5b36b3e047b182214e84
Joachim Schurig <jschurig@gmx.net>
2022-05-15 18:03:05 +0200

    build jemalloc from source with profiler support

--- cmake/modules/FindJEMalloc.cmake
+++ cmake/modules/FindJEMalloc.cmake
@@ -1,6 +1,13 @@
+if (EXISTS /opt/jemalloc-prof/)
+	# this is our own build with profiling enabled
+	find_path(JEMalloc_INCLUDE_DIR NO_DEFAULT_PATH PATHS /opt/jemalloc-prof/include NAMES jemalloc/jemalloc.h )
+	find_library(JEMalloc_LIBRARY NO_DEFAULT_PATH PATHS /opt/jemalloc-prof/lib NAMES jemalloc )
+else()
+	# search a stock library
+	find_path(JEMalloc_INCLUDE_DIR NAMES jemalloc/jemalloc.h)
+	find_library(JEMalloc_LIBRARY NAMES jemalloc)
+endif()
 
-find_path(JEMalloc_INCLUDE_DIR NAMES jemalloc/jemalloc.h)
-find_library(JEMalloc_LIBRARY NAMES jemalloc)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(JEMalloc DEFAULT_MSG JEMalloc_LIBRARY JEMalloc_INCLUDE_DIR)
 

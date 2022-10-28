2214fea00aaf5df69461634fe1fe3e6408848e6b
Joachim Schurig <jschurig@gmx.net>
2022-10-28 15:04:03 +0200

    fixing email detection regex to be case insensitive

--- kstringutils.cpp
+++ kstringutils.cpp
@@ -246,16 +246,13 @@ bool kIsFloat(KStringView str, KStringView::value_type chDecimalSeparator) noexc
 bool kIsEmail(KStringView str) noexcept
 //-----------------------------------------------------------------------------
 {
-	if (str.empty())
-	{
-		return false;
-	}
-
  	// see https://stackoverflow.com/a/201378
+	// but note that we extended the regex to match uppercase ASCII alphabetic characters
+	// as well, as that is what RFC5322 and RFC1035 permit (addresses are caseless)
 	static constexpr KStringView sRegex =
-	R"foo((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))foo";
+	R"foo((?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))foo";
 
-	return KRegex::Matches(str.ToLower(), sRegex);
+	return KRegex::Matches(str, sRegex);
 
 } // kIsEmail
 

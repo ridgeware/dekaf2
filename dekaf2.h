45d6eeea8a7c6375e3d1d6bb1e5c031a7211ef1f
Joachim Schurig <jschurig@gmx.net>
2023-04-04 17:54:33 +0200

    fixing arm atomic

--- dekaf2.h
+++ dekaf2.h
@@ -286,7 +286,7 @@ private:
 	KTimer::ID_t m_OneSecTimerID;
 	std::mutex m_OneSecTimerMutex;
 	std::vector<OneSecCallback> m_OneSecTimers;
-#if (DEKAF2_IS_GCC && DEKAF2_GCC_VERSION_MAJOR < 9) || DEKAF2_BITS < 64
+#if (DEKAF2_IS_GCC && DEKAF2_GCC_VERSION_MAJOR < 9)
 	KUnixTime m_iCurrentTime;
 #else
 	std::atomic<KUnixTime> m_iCurrentTime;

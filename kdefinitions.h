01dbba6e05d861084f1299510dc246101a730a77
Joachim Schurig <jschurig@gmx.net>
2024-02-08 22:15:59 +0100

    make use of std::source_location if available

--- kdefinitions.h
+++ kdefinitions.h
@@ -135,7 +135,10 @@
 	#define DEKAF2_GLIBC_VERSION_MINOR __GLIBC_MINOR__
 #endif
 
-#if defined __GNUC__
+#if DEKAF2_HAS_INCLUDE(<source_location>)
+	#include <source_location>
+	#define DEKAF2_FUNCTION_NAME std::source_location::current().function_name()
+#elif defined __GNUC__
 	#define DEKAF2_FUNCTION_NAME __PRETTY_FUNCTION__
 #elif defined _MSC_VER
 	#define DEKAF2_FUNCTION_NAME __FUNCSIG__

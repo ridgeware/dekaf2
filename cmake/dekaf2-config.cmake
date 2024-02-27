5eff6a9ff966f20593b26a3b8d3dc7993c6ab9b2
Joachim Schurig <jschurig@gmx.net>
2024-02-27 17:59:54 +0100

    adding RelWithDebInfo build type

--- cmake/dekaf2-config.cmake
+++ cmake/dekaf2-config.cmake
@@ -5,6 +5,16 @@ if (NOT CMAKE_BUILD_TYPE)
 	set(CMAKE_BUILD_TYPE "Release")
 endif()
 
+# we only support the Release and Debug output locations - other
+# build types have to pick either or
+if (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+	set(DEKAF2_OUTPUT_PATH "Release")
+elseif(CMAKE_BUILD_TYPE STREQUAL "debug")
+	set(DEKAF2_OUTPUT_PATH "Debug")
+else()
+	set(DEKAF2_OUTPUT_PATH ${CMAKE_BUILD_TYPE})
+endif()
+
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/dekaf2.cmake)
-include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/dekaf2-setup.cmake)
+include(${SELF_DIR}/${DEKAF2_OUTPUT_PATH}/dekaf2.cmake)
+include(${SELF_DIR}/${DEKAF2_OUTPUT_PATH}/dekaf2-setup.cmake)

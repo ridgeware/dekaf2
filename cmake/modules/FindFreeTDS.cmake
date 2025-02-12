5cf9bd7e0637e381b0ac0a42902ea67de59d4423
Joachim Schurig <jschurig@translations.com>
2025-02-12 13:19:16 +0100

    we only need libct, not libdb

--- cmake/modules/FindFreeTDS.cmake
+++ cmake/modules/FindFreeTDS.cmake
@@ -1,7 +1,9 @@
 find_path(FREETDS_INCLUDE_DIR NAMES ctpublic.h)
 find_library(FREETDS_LIBRARIES_CT NAMES ct)
-find_library(FREETDS_LIBRARIES_SYBDB NAMES sybdb)
-set(FREETDS_LIBRARIES ${FREETDS_LIBRARIES_CT} ${FREETDS_LIBRARIES_SYBDB})
+set(FREETDS_LIBRARIES ${FREETDS_LIBRARIES_CT})
+# we only need libct, but if you'd like to use libdb or libodbc, include them like below
+# find_library(FREETDS_LIBRARIES_SYBDB NAMES sybdb)
+# set(FREETDS_LIBRARIES ${FREETDS_LIBRARIES} ${FREETDS_LIBRARIES_SYBDB})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FreeTDS DEFAULT_MSG FREETDS_LIBRARIES FREETDS_INCLUDE_DIR)
 

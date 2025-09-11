479038e4144a6da113b42a3979f512e0573abcbb
Joachim Schurig <jschurig@translations.com>
2025-09-11 23:49:19 +0200

    fix KUUID on windows

--- kdefinitions.h
+++ kdefinitions.h
@@ -195,10 +195,6 @@
 		#define NOMINMAX
 	#endif
 
-	#ifndef WIN32_LEAN_AND_MEAN
-		#define WIN32_LEAN_AND_MEAN
-	#endif
-
 	#ifndef _CRT_SECURE_NO_WARNINGS
 		#define _CRT_SECURE_NO_WARNINGS
 	#endif

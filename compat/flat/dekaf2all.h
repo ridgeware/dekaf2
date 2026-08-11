5683b78cbbf041533396f6b9957c183df8d9ba01
Joachim Schurig <jschurig@translations.com>
2026-08-11 17:06:45 +0200

    rebuilt dekaf2all.h

--- compat/flat/dekaf2all.h
+++ compat/flat/dekaf2all.h
@@ -46,18 +46,22 @@
 /// globbed include of all dekaf2 header files
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// refresh by running (and capturing):
-//   ls -1 *.h | awk 'BEGIN {print "#include \"dekaf2.h\""} ($0 !~ /^dekaf2/) {printf ("#include \"%s\"\n", $0)}'
+// refresh by running (and capturing) from the repo root - dekaf2.h stays first:
+//   find source -name '*.h' -not -path '*/bits/*' | sed 's|^source|dekaf2|' \
+//   | awk -F/ '$NF !~ /^dekaf2/ { print $NF "\t#include <" $0 ">" }' | sort | cut -f2
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #include <dekaf2/core/init/dekaf2.h>
 #include <dekaf2/core/types/kaddrplus.h>
 #include <dekaf2/crypto/cipher/kaes.h>
 #include <dekaf2/containers/memory/kallocator.h>
+#include <dekaf2/web/push/kapplepush.h>
+#include <dekaf2/containers/memory/karenaallocator.h>
 #include <dekaf2/crypto/cipher/karia.h>
 #include <dekaf2/containers/associative/kassociative.h>
 #include <dekaf2/threading/primitives/katomic_object.h>
 #include <dekaf2/crypto/auth/kawsauth.h>
 #include <dekaf2/core/format/kbar.h>
+#include <dekaf2/crypto/encoding/kbase32.h>
 #include <dekaf2/crypto/encoding/kbase64.h>
 #include <dekaf2/crypto/auth/kbcrypt.h>
 #include <dekaf2/core/types/kbit.h>
@@ -76,23 +80,26 @@
 #include <dekaf2/http/protocol/kchunkedtransfer.h>
 #include <dekaf2/core/init/kcompatibility.h>
 #include <dekaf2/io/compression/kcompression.h>
+#include <dekaf2/data/json/kconfig.h>
 #include <dekaf2/rest/limits/kconnectionlimiter.h>
 #include <dekaf2/http/cookie/kcookie.h>
 #include <dekaf2/io/streams/kcountingstreambuf.h>
 #include <dekaf2/core/errors/kcrashexit.h>
 #include <dekaf2/crypto/hash/kcrc.h>
 #include <dekaf2/data/csv/kcsv.h>
-#include <dekaf2/crypto/ec/keckey.h>
-#include <dekaf2/crypto/ec/kecsign.h>
 #include <dekaf2/core/types/kctype.h>
 #include <dekaf2/io/pipes/kdataconsumer.h>
 #include <dekaf2/io/pipes/kdataprovider.h>
 #include <dekaf2/time/clock/kdate.h>
 #include <dekaf2/core/init/kdefinitions.h>
+#include <dekaf2/util/text/kdiff.h>
 #include <dekaf2/net/udp/kdtlsserver.h>
 #include <dekaf2/net/udp/kdtlssocket.h>
-#include <dekaf2/util/text/kdiff.h>
+#include <dekaf2/net/udp/kdtlsstream.h>
 #include <dekaf2/time/duration/kduration.h>
+#include <dekaf2/crypto/ec/keckey.h>
+#include <dekaf2/crypto/ec/kecsign.h>
+#include <dekaf2/crypto/ec/ked25519sign.h>
 #include <dekaf2/crypto/encoding/kencode.h>
 #include <dekaf2/core/types/keraseremove.h>
 #include <dekaf2/core/errors/kerror.h>
@@ -103,15 +110,18 @@
 #include <dekaf2/core/format/kformat.h>
 #include <dekaf2/core/format/kformtable.h>
 #include <dekaf2/core/types/kfrozen.h>
+#include <dekaf2/util/codec/kgaloisfield.h>
+#include <dekaf2/net/geo/kgeoip.h>
 #include <dekaf2/system/os/kgetruntimestack.h>
 #include <dekaf2/crypto/hash/khash.h>
 #include <dekaf2/system/os/kheapmon.h>
 #include <dekaf2/crypto/encoding/khex.h>
-#include <dekaf2/crypto/kdf/khkdf.h>
 #include <dekaf2/containers/sequential/khistory.h>
+#include <dekaf2/crypto/kdf/khkdf.h>
 #include <dekaf2/crypto/hash/khmac.h>
 #include <dekaf2/web/html/khtmlcontentblocks.h>
 #include <dekaf2/web/html/khtmldom.h>
+#include <dekaf2/web/html/khtmldom_cursor.h>
 #include <dekaf2/web/html/khtmlentities.h>
 #include <dekaf2/web/html/khtmlparser.h>
 #include <dekaf2/http/protocol/khttp2.h>
@@ -137,7 +147,6 @@
 #include <dekaf2/net/address/kipaddress.h>
 #include <dekaf2/net/address/kipnetwork.h>
 #include <dekaf2/core/strings/kjoin.h>
-#include <dekaf2/data/json/kconfig.h>
 #include <dekaf2/data/json/kjson.h>
 #include <dekaf2/data/json/kjson2.h>
 #include <dekaf2/time/clock/kjuliandate.h>
@@ -146,7 +155,9 @@
 #include <dekaf2/core/logging/klog.h>
 #include <dekaf2/core/logging/klogrotate.h>
 #include <dekaf2/util/mail/kmail.h>
+#include <dekaf2/util/mail/kmailspool.h>
 #include <dekaf2/core/types/kmakecopyable.h>
+#include <dekaf2/io/readwrite/kmemorymap.h>
 #include <dekaf2/crypto/hash/kmessagedigest.h>
 #include <dekaf2/web/url/kmime.h>
 #include <dekaf2/io/streams/kmodifyingstreambuf.h>
@@ -155,6 +166,8 @@
 #include <dekaf2/net/address/knetworkinterface.h>
 #include <dekaf2/util/text/kngram.h>
 #include <dekaf2/crypto/auth/kopenid.h>
+#include <dekaf2/crypto/auth/kopenidclient.h>
+#include <dekaf2/crypto/auth/kopenidserver.h>
 #include <dekaf2/util/cli/koptions.h>
 #include <dekaf2/io/pipes/koutpipe.h>
 #include <dekaf2/data/template/koutputtemplate.h>
@@ -168,12 +181,14 @@
 #include <dekaf2/time/duration/kprof.h>
 #include <dekaf2/containers/associative/kprops.h>
 #include <dekaf2/system/process/kpty.h>
+#include <dekaf2/util/qrcode/kqrcode.h>
 #include <dekaf2/net/quic/kquicstream.h>
 #include <dekaf2/crypto/encoding/kquotedprintable.h>
 #include <dekaf2/crypto/random/krandom.h>
 #include <dekaf2/rest/limits/kratelimiter.h>
 #include <dekaf2/io/readwrite/kread.h>
 #include <dekaf2/io/readwrite/kreader.h>
+#include <dekaf2/util/codec/kreedsolomon.h>
 #include <dekaf2/core/types/kreference_proxy.h>
 #include <dekaf2/core/strings/kregex.h>
 #include <dekaf2/data/template/kreplacer.h>
@@ -182,6 +197,7 @@
 #include <dekaf2/rest/framework/krestclient.h>
 #include <dekaf2/rest/framework/krestroute.h>
 #include <dekaf2/rest/framework/krestserver.h>
+#include <dekaf2/rest/framework/krestsession.h>
 #include <dekaf2/time/scheduler/kron.h>
 #include <dekaf2/data/sql/krow.h>
 #include <dekaf2/crypto/rsa/krsa.h>
@@ -189,6 +205,8 @@
 #include <dekaf2/crypto/rsa/krsakey.h>
 #include <dekaf2/crypto/rsa/krsasign.h>
 #include <dekaf2/core/types/kscopeguard.h>
+#include <dekaf2/system/os/kservice.h>
+#include <dekaf2/crypto/auth/ksession.h>
 #include <dekaf2/system/shared/ksharedmemory.h>
 #include <dekaf2/containers/memory/ksharedptr.h>
 #include <dekaf2/containers/memory/ksharedref.h>
@@ -196,6 +214,7 @@
 #include <dekaf2/util/mail/ksmtp.h>
 #include <dekaf2/data/template/ksnippets.h>
 #include <dekaf2/core/errors/ksourcelocation.h>
+#include <dekaf2/containers/sequential/kspan.h>
 #include <dekaf2/core/strings/ksplit.h>
 #include <dekaf2/data/sql/ksql.h>
 #include <dekaf2/data/sql/ksqlite.h>
@@ -224,6 +243,7 @@
 #include <dekaf2/time/series/ktimeseries.h>
 #include <dekaf2/net/tls/ktlscontext.h>
 #include <dekaf2/net/tls/ktlsstream.h>
+#include <dekaf2/crypto/auth/ktotp.h>
 #include <dekaf2/net/util/ktunnel.h>
 #include <dekaf2/net/udp/kudpserver.h>
 #include <dekaf2/net/udp/kudpsocket.h>
@@ -246,9 +266,11 @@
 #include <dekaf2/rest/serving/kwebserverpermissions.h>
 #include <dekaf2/http/websocket/kwebsocket.h>
 #include <dekaf2/http/websocket/kwebsocketclient.h>
+#include <dekaf2/web/ui/kwebui.h>
 #include <dekaf2/core/strings/kwords.h>
 #include <dekaf2/io/readwrite/kwrite.h>
 #include <dekaf2/io/readwrite/kwriter.h>
+#include <dekaf2/crypto/ec/kx25519.h>
 #include <dekaf2/data/xml/kxml.h>
 #include <dekaf2/util/cli/kxterm.h>
 #include <dekaf2/util/archive/kzip.h>
